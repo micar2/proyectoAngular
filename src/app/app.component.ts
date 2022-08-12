56e0698edb6c8db54d1b6903c1134b1d151971ff
Mic4r2 <109005822+Mic4r2@users.noreply.github.com>
2022-08-12 22:35:22 +0200

    primera visualización

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'proyectosCliente';
+export class AppComponent implements OnInit{
+  proyectos: any[] = [];
+
+  constructor(
+    private appService: AppService
+  ){ }
+  ngOnInit(): void {
+    this.appService.getAll().subscribe((data: any) => this.proyectos = data);
+  }
 }
