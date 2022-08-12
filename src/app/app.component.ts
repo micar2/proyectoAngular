ffc8727a92c0989c45f47578e33c4296fe768bdf
Mic4r2 <109005822+Mic4r2@users.noreply.github.com>
2022-08-12 23:29:07 +0200

    final de la funcionalidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,46 @@ import { AppService } from './app.service';
 })
 export class AppComponent implements OnInit{
   proyectos: any[] = [];
+  proyecto = {
+    id:null,
+    nombre:'',
+    completado:false
+  }
 
   constructor(
     private appService: AppService
   ){ }
+
   ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll(){
     this.appService.getAll().subscribe((data: any) => this.proyectos = data);
+  }
+
+  save(){
+    if(this.proyecto.id){
+      this.appService.update(this.proyecto.id,this.proyecto).subscribe(() => this.getAll());
+    }else{
+      this.appService.create(this.proyecto).subscribe(() => this.getAll());
+    }
+    
+
+    this.proyecto = {
+      id:null,
+      nombre:'',
+      completado:false
+    }
+  }
+
+  edit(proyecto: any){
+    this.proyecto = {
+      ...proyecto
+    }
+  }
+
+  delete(proyecto: any){
+    this.appService.delete(proyecto.id, proyecto).subscribe(() => this.getAll());
   }
 }
